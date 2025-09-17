c8df5886527138ac01c0c2eef421f25000d4e949
lucatak <contato.lucas.santos@hotmail.com>
2025-09-16 21:00:31 -0300

    retira coments sujios

--- aula3/ex1.c
+++ aula3/ex1.c
@@ -1,7 +1,7 @@
-// exercicio1.c
+// ex1.c
 // Cria 23 threads. Todas compartilham a variável 'letra'.
 // Cada thread só imprime quando 'letra' == sua vez; depois incrementa e acorda a próxima.
-// Implementa um "monitor simples" com mutex + variável de condição.
+// Implementa um "monitor simples" com mutex + variável de condição
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,36 +9,33 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-#define NUM_THREADS 23 // troque para 23 se o professor exigir exatamente 23
+#define NUM_THREADS 23
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-char letra_compartilhada = 'A';   // variável compartilhada 'letra'
+char letra_compartilhada = 'A';
 int  total_para_imprimir = NUM_THREADS;
 
 typedef struct {
-  int indice; // 0..N-1 (A..Z)
+  int indice; 
 } DadosThread;
 
 void* rotina_impressao(void* arg) {
+
   DadosThread* dados = (DadosThread*)arg;
   char minha_letra = 'A' + dados->indice;
 
   pthread_mutex_lock(&mutex);
   while (letra_compartilhada != minha_letra) {
-    // LOG: não é minha vez ainda
     printf("[t%02d] Aguardando: minha_letra=%c, atual=%c\n",
            dados->indice, minha_letra, letra_compartilhada);
     pthread_cond_wait(&cond, &mutex);
   }
 
-  // É a minha vez
   printf("[t%02d] >>> IMPRIMINDO letra: %c\n", dados->indice, minha_letra);
-  // Simula algum trabalho
   usleep(20 * 3000);
 
-  // Incrementa para a próxima thread/letra
   letra_compartilhada++;
   total_para_imprimir--;
   printf("[t%02d] Incrementou 'letra' para: %c | faltam: %d\n",
